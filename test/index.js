8e4d274caa58fb8bac57417eba7613c300622c25
thinkpad-fei <451231623@qq.com>
2018-10-22 10:39:08 +0800

    bug

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ let img=new Watermarkimg(500,500,bakColor);
 img
     //.drawImg(path.resolve(__dirname,'./800x600.jpg'))
     //.drawImg(path.resolve(__dirname,'./600x800.jpg'))
-    .drawImg(path.resolve(__dirname,'./400x400.jpg'))
+    .drawImg(path.resolve(__dirname,'./abc.png'))
     //.drawImg(path.resolve(__dirname,'./watermarkImg.png'))
     .drawImg(path.resolve(__dirname,'./logo.png'))
     .save(path.resolve(__dirname,'./a.png'))
