e2479f8e5366889134a8c26779bdb058ffff5c88
thinkpad-fei <451231623@qq.com>
2018-10-22 09:54:16 +0800

    bug

--- index.js
+++ index.js
@@ -17,52 +17,29 @@ class Watermarkimg{
     }
     drawImg(sourceImg){
         let img = images(sourceImg);
-        let {w,h}=this._resize(img.width(),img.height());;
-
+        let imgW=img.width();
+        let imgH=img.height();
+        this.imgWH=imgW/imgH;
+        let w=imgW;
+        let h=imgH;
+        if(imgW>imgH||(imgW==imgH&&this._canvasWidth<this._canvasHeight)){
+            w=this._canvasWidth;
+            h=Math.ceil(w/this.imgWH);
+        }else if(imgW<imgH||(imgW==imgH&&this._canvasWidth>this._canvasHeight)||(this._canvasWidth==this._canvasHeight)){
+            h=this._canvasHeight;
+            w=Math.ceil(h*this.imgWH);
+        }
         img.size(w, h);
         let x=(this._canvasWidth-w)/2,y=(this._canvasHeight-h)/2;
-        if(isDebug) console.log('drawImg _resize w:'+w+' h:'+h+' x:'+x+' y:'+y)
+        if(isDebug){
+            console.log(`_canvasWidth:${this._canvasWidth},canvasHeight:${this._canvasHeight}`);
+            console.log('drawImg _resize w:'+w+' h:'+h+' x:'+x+' y:'+y)
+        }
         this._canvas.draw(img, x, y)
         return this;
     }
     save(filepath){
         this._canvas.save(filepath);
-    }
-    _resize(imgW,imgH){
-        if(isDebug) console.log('img w:'+imgW+' h:'+imgH+' resize to:');
-        if (this.imgWH==0){
-            this.imgWH=imgW/imgH
-        }
-        let imgWH=this.imgWH;
-        let endW=imgW;
-        let endH=imgH;
-        if (imgW>imgH){
-            if (this._canvasWidth>this._canvasHeight){
-                if (imgW>this._canvasWidth){
-                    endW=this._canvasWidth;
-                    endH=Math.floor(endW*imgWH);
-                }
-            }else{
-                if (imgH>this._canvasHeight){
-                    endH=this._canvasHeight;
-                    endW=Math.floor(endH/imgWH);
-                }
-            }
-
-        }else{
-            if (this._canvasHeight>this._canvasWidth){
-                if (imgH>this._canvasHeight){
-                    endH=this._canvasHeight;
-                    endW=Math.floor(endH/imgWH);
-                }
-            }else{
-                if (imgW>this._canvasWidth){
-                    endW=this._canvasWidth;
-                    endH=Math.floor(endW*imgWH);
-                }
-            }
-        }
-        return {w:endW,h:endH}
     }
 }
 module.exports=Watermarkimg;
--- test/index.js
+++ test/index.js
@@ -2,10 +2,11 @@ const path = require('path')
 const Watermarkimg=require('../index');
 
 let bakColor=[0xff,0xff,0xff,1];
-let img=new Watermarkimg(210,154,bakColor);
+let img=new Watermarkimg(500,500,bakColor);
 img
     //.drawImg(path.resolve(__dirname,'./800x600.jpg'))
-    .drawImg(path.resolve(__dirname,'./600x800.jpg'))
-    .drawImg(path.resolve(__dirname,'./watermarkImg.png'))
+    //.drawImg(path.resolve(__dirname,'./600x800.jpg'))
+    .drawImg(path.resolve(__dirname,'./400x400.jpg'))
+    //.drawImg(path.resolve(__dirname,'./watermarkImg.png'))
     .drawImg(path.resolve(__dirname,'./logo.png'))
     .save(path.resolve(__dirname,'./a.png'))
