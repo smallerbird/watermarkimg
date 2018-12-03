b5ae3820dad788b34874d1be642fdf342c1dbbdd
thinkpad-fei <451231623@qq.com>
2018-12-03 20:58:46 +0800

    test

--- test/index.js
+++ test/index.js
@@ -4,9 +4,9 @@ const Watermarkimg=require('../index');
 let bakColor=[0xff,0xff,0xff,1];
 let img=new Watermarkimg(500,500,bakColor);
 img
-    //.drawImg(path.resolve(__dirname,'./800x600.jpg'))
+    .drawImg(path.resolve(__dirname,'./800x600.jpg'))
     //.drawImg(path.resolve(__dirname,'./600x800.jpg'))
-    .drawImg(path.resolve(__dirname,'./abc.png'))
+    //.drawImg(path.resolve(__dirname,'./400x400.jpg'))
     //.drawImg(path.resolve(__dirname,'./watermarkImg.png'))
     .drawImg(path.resolve(__dirname,'./logo.png'))
     .save(path.resolve(__dirname,'./a.png'))
